d38d83ddb69c2bca876f01d3dd79d5e9bf2d1462
Evgeniy Chernyshenko <magdegaben@gmail.com>
2020-12-16 12:35:52 +0200

    ading code

---
+++ src/JS/colorChange.js
@@ -0,0 +1,49 @@
+import refsBtn from './refs.js';
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+  '#FFF45F',
+  '#219LF3',
+  '#4WDF50',
+  '#C71585',
+  '#E0FFFF',
+  '#0000CD',
+  '#A0522D',
+];
+
+refsBtn.start.addEventListener('click', onStart);
+refsBtn.stop.addEventListener('click', onStop);
+
+let intervalId = null;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function startChangingColor(cls, index) {
+  refsBtn.body.style.backgroundColor = cls[index];
+}
+
+function onStart() {
+  if (intervalId) return;
+
+  intervalId = setInterval(() => {
+    let randomInteger = randomIntegerFromInterval(0, colors.length - 1);
+    startChangingColor(colors, randomInteger);
+  }, 1000);
+}
+
+function onStop() {
+  clearInterval(intervalId);
+  cleanScreen();
+  intervalId = null;
+}
+
+const cleanScreen = () => {
+  refsBtn.body.style.backgroundColor = '';
+};
